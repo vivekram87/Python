44c702a77d26d2a0da8ff0677ff3753c0b59d2d3
vivekram87 <vivram214@gmail.com>
2022-05-23 11:06:47 +0530

    all_checks.py modified

--- all_checks.py
+++ all_checks.py
@@ -1,10 +1,13 @@
+import os
+import sys
 
 def check_reboot():
     # Return true if the computer is pending a reboot
     return os.path.exist("run/reboot-required")
 def main():
-    print("Hello World")
-    pass
+    if check_reboot():
+        print("Pending Reboot.")
+        sys.exit(1)
 
 main()
 
