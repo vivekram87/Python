f507c8456c65bac41b73e34911b6907fcb43abeb
vivekram87 <61583051+vivekram87@users.noreply.github.com>
2022-06-28 02:01:08 +0530

    update

--- disk_usage.py
+++ disk_usage.py
@@ -11,6 +11,19 @@ def check_disk_usage(disk, min_gb, min_percent):
     if percent_free < min_percent or gigabytes_free < min_percent:
         return False
     return True
+def check_reboot_root():
+        print("Pending Reboot.")
+        """Returns true if the root partition is full, False otherwise"""
+        return check_disk_full(disk="/", min_gb=0.1, min_percent=1)
+    def main():
+        if check_reboot():
+            print("Pending Reboot")
+            sys.exit(1)
+            
+        print("Everything is ok")
+        sys.exit(0)
+        
+     main()    
 
 # check for 2 gb free or 10% free space
 
