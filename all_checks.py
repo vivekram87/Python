18089dccd82d3e97dfda07fe159075fce26c54a7
vivekram87 <61583051+vivekram87@users.noreply.github.com>
2022-07-05 21:25:09 +0530

    def check_no_network():

--- all_checks.py
+++ all_checks.py
@@ -9,7 +9,16 @@ def main():
         print("Pending Reboot.")
         sys.exit(1)
     print("Everything is ok!")
+    check_no_network()
     sys.exit(0)
+    
+def check_no_network():
+    '''Return True if it fails to resolve Google.com, else False'''
+    try:
+        socket.gethostbyname("www.google.com")
+        return False
+    except:
+        return True
 
 main()
 
