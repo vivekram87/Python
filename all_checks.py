36691165db81fba278ede0ed1f2dda75f7ec89a3
vivekram87 <61583051+vivekram87@users.noreply.github.com>
2022-07-05 21:28:51 +0530

    1

--- all_checks.py
+++ all_checks.py
@@ -5,11 +5,11 @@ def check_reboot():
     # Return true if the computer is pending a reboot
     return os.path.exists("run/reboot-required")
 def main():
+    check_no_network()
     if check_reboot():
         print("Pending Reboot.")
         sys.exit(1)
     print("Everything is ok!")
-    check_no_network()
     sys.exit(0)
     
 def check_no_network():
